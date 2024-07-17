ee9eafe2bc197cbaaa6526572071ae71d0d2242b
Bhakti K <48476445+kbhakti@users.noreply.github.com>
2024-07-17 21:36:29 +0530

    Update Ethical_AI_Framework.py

--- Ethical_AI_Framework.py
+++ Ethical_AI_Framework.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 # Set the title of the page
-st.title("Simple Streamlit UI")
+st.title("Ethical-AI-Framework-for-Content-Creation")
 
 # Add a heading
-st.header("Enter your text below")
+st.header("Enter the text below")
 
 # Create a text input widget
 user_input = st.text_input("Your Text:")
